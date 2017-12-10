897c8188feba9533042983e2c4e1f277bca02e69
hym_dn <yameng_he@aliyun.com>
2017-12-10 12:41:58 +0800

    提交17-2源码

---
+++ Chapter17/17-2/main.cpp
@@ -0,0 +1,10 @@
+#include"../../apue.h"
+
+/**
+ * Returns a STREAMS-based pipe,width the two file descriptors
+ * returned in fd[0] and fd[1].
+ */
+int s_pipe(int fd[2]){
+    return(pipe(fd));
+}
+
