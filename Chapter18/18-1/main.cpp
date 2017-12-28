30a995754d00affce8206ac492259ab3b2f246e9
hym_dn <yameng_he@aliyun.com>
2017-12-28 22:17:09 +0800

    提交源码18-1

---
+++ Chapter18/18-1/main.cpp
@@ -0,0 +1,30 @@
+#include"../../apue.h"
+#include<termios.h>
+
+int main(void){
+    //定义终端
+    struct termios term;
+    //_PC_VDISABLE值
+    long vdisable;
+    //检测标准输入是否为终端设备
+    if(isatty(STDIN_FILENO)==0){
+        err_quit("standard input is not a terminal device");
+    }
+    //获取标准输入的_PC_VDISABLE值
+    if((vdisable=fpathconf(STDIN_FILENO,_PC_VDISABLE))<0){
+        err_quit("pfathconf error or _POSIX_VDISABLE not in effect");
+    }
+    //获取标准输入的终端信息结构
+    if(tcgetattr(STDIN_FILENO,&term)<0){
+        err_sys("tcgetattr error");
+    }
+    //修改终端结构
+    term.c_cc[VINTR]=vdisable;//禁用中断键
+    term.c_cc[VEOF]=2;//将文件结束符设置为Control-B
+    //重新设置终端信息结构
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&term)<0){
+        err_sys("tcsetattr error");
+    }
+    //退出
+    exit(0);
+}
