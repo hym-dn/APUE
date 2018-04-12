686f21da84a62074672c4c51d7e732551dcece23
hym_dn <yameng_he@aliyun.com>
2018-04-12 21:26:24 +0800

    提交3-5源码

--- Chapter3/3-5/main.cpp
+++ Chapter3/3-5/main.cpp
@@ -14,3 +14,17 @@ void set_fl(int fd,int flags){
         err_sys("fcntl F_SETFL error");
     }
 }
+
+void clr_fl(int fd,int flags){
+    int val;
+    // 获取文件描述符状态标志
+    if((val=fcntl(fd,F_GETFL,0))<0){
+        err_sys("fcntl F_GETFL error");
+    }
+    // 关闭标志
+    val&=~flags; /* turn on flags */
+    // 设置标志
+    if(fcntl(fd,F_SETFL,val)<0){
+        err_sys("fcntl F_SETFL error");
+    }
+}
