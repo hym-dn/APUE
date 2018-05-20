bf2bd343b679e9dea4c48592b3c988b8111e27c5
yameng_he <yameng_he@aliyun.com>
2018-05-20 11:55:50 +0800

    提交源码7-7

---
+++ Chapter7/7-7/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define DATAFILE "datafile"
+
+FILE *open_data(void){
+    FILE *fp;
+    char databuf[BUFSIZE]; /* setvbuf makes this the stdio buffer */
+    if((fp=fopen(DATAFILE,"r"))==NULL){
+        return(NULL);
+    }
+    if(setvbuf(fp,databuf,_IOLBF,BUFSIZE)!=0){
+        return(NULL);
+    }
+    return(fp); /* error */
+}
