edb0f8a45fa65f3dc9fd7164cb35bfca8f5df036
hym_dn <yameng_he@aliyun.com>
2018-05-26 11:51:46 +0800

    提交源码8-17

--- Chapter8/8-17/main.c
+++ Chapter8/8-17/main.c
@@ -23,10 +23,46 @@ static unsigned long compt2ulong(comp_t comptime){ /* convert comp_t to unsigned
     val=comptime&0x1fff; /* 13-bit fraction */
     // 3位指数
     exp=(comptime>>13)&7; /* 3-bit exponent (0-7) */
-    // 计算值
+    // 指数换算
     while(exp-->0){
         val*=8;
     }
     // 返回值
     return(val);
+}
+
+int main(int argc,char *argv[]){
+    // 进程会计信息
+    struct acct acdata;
+    // 文件流
+    FILE *fp;
+    // 输入参数个数非法
+    if(argc!=2){
+        err_quit("usage: pracct filename");
+    }
+    // 打开指定文件
+    if((fp=fopen(argv[1],"r"))==NULL){
+        err_sys("can't open %s",argv[1]);
+    }
+    // 从文件中读取进程会计信息
+    while(fread(&acdata,sizeof(acdata),1,fp)==1){ // 读取成功
+        // 打印进程会计信息
+        printf(FMT,(int)sizeof(acdata.ac_comm),(
+            int)sizeof(acdata.ac_comm),acdata.ac_comm,
+            compt2ulong(acdata.ac_etime),compt2ulong(
+            acdata.ac_io),
+#ifdef HAS_SA_STAT
+            (unsigned char)acdata.ac_stat,
+#endif
+            acdata.ac_flag & ACORE ? 'D' : ' ',
+            acdata.ac_flag & AXSIG ? 'X' : ' ',
+            acdata.ac_flag & AFORK ? 'F' : ' ',
+            acdata.ac_flag & ASU   ? 'S' : ' ');
+    }
+    // 文件出错
+    if(ferror(fp)){
+        err_sys("read error");
+    }
+    // 退出
+    exit(0);
 }
