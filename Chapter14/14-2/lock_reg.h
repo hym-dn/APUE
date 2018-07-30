fcfec27ccf8def34a6f06031e5335dfe56c16124
yameng_he <yameng_he@aliyun.com>
2018-07-30 12:02:56 +0800

    提交源码14-2

--- Chapter14/14-2/lock_reg.h
+++ Chapter14/14-2/lock_reg.h
@@ -15,3 +15,17 @@ int lock_reg(int fd,int cmd,int type,off_t offset,int whence,off_t len){
     // 上锁
     return(fcntl(fd,cmd,&lock));
 }
+
+// 加读锁
+#define read_lock(fd,offset,whence,len) \
+    lock_reg((fd),F_SETLK,F_RDLCK,(offset),(whence),(len))
+#define readw_lock(fd,offset,whence,len) \
+    lock_reg((fd),F_SETLKW,F_RDLCK,(offset),(whence),(len))
+// 加写锁
+#define write_lock(fd,offset,whence,len) \
+    lock_reg((fd),F_SETLK,F_WRLCK,(offset),(whence),(len))
+#define writew_lock(fd,offset,whence,len) \
+    lock_reg((fd),F_SETLKW,F_WRLCK,(offset),(whence),(len))
+// 解锁
+#define un_lock(fd,offset,whence,len) \
+    lock_reg((fd),F_SETLK,F_UNLCK,(offset),(whence),(len))
