410627bdd80bfc9a8c7a0affbe3708ad2fa0ab3c
yameng_he <yameng_he@aliyun.com>
2018-08-01 09:56:55 +0800

    提交源码14-6

--- Chapter14/14-6/main.c
+++ Chapter14/14-6/main.c
@@ -1,4 +1,4 @@
-#include"apue.h"
+#include"../../apue.h"
 #include<errno.h>
 #include<fcntl.h>
 #include<sys/wait.h>
@@ -34,7 +34,48 @@ int main(int argc,char *argv[]){
     }
     // 初始化进程通信机制
     TELL_WAIT();
-
-
-
+    // 创建父进程
+    if((pid=fork())<0){
+        err_sys("fork error");
+    }
+    // 父进程
+    else if(pid>0){ /* parent */
+        // 在文件中加锁
+        /* write lock entire file */
+        if(write_lock(fd,0,SEEK_SET,0)<0){
+            err_sys("write_lock error");
+        }
+        // 向子进程发送信号
+        TELL_CHILD(pid);
+        // 等待子进程结束
+        if(waitpid(pid,NULL,0)<0){
+            err_sys("waitpid error");
+        }
+    }
+    // 子进程
+    else{ /* child */
+        // 等待父进程加锁
+        WAIT_PARENT(); /* wait for parent to set lock */
+        // 设置文件为非阻塞
+        set_fl(fd,O_NONBLOCK);
+        // 检测读锁
+        /* first let's see what error we get if region is locked */
+        if(read_lock(fd,0,SEEK_SET,0)!=-1){ /* no wait */
+            err_sys("child: read_lock succeeded");
+        }
+        // 打印信息
+        printf("read_lock of already-locked region returns %d\n",errno);
+        // 尝试读取
+        /* now try to read the mandatory locked file */
+        if(lseek(fd,0,SEEK_SET)==-1){
+            err_sys("lseek error");
+        }
+        if(read(fd,buf,2)<0){ // 强制性锁有效
+            err_ret("read failed (mandatory locking works)");
+        }else{ // 非强制性锁无效
+            printf("read OK (no mandatory locking),buf=%2.2s\n",buf);
+        }
+    }
+    // 退出
+    exit(0);
 }
