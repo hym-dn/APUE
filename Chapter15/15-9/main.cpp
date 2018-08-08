ca47a9f96fe8f22a86d05ecd626099cf7fecb060
yameng_he <yameng_he@aliyun.com>
2018-08-08 17:31:43 +0800

    提交源码15-9

--- Chapter15/15-9/main.cpp
+++ Chapter15/15-9/main.cpp
@@ -1,69 +1,69 @@
 #include"../../apue.h"
 
-//信号捕捉函数
-static void sig_pipe(int);
+// 信号捕捉函数
+static void sig_pipe(int); /* our signal handler */
 
 int main(void){
-    //数量、管道
+    // 数量,管道
     int n,fd1[2],fd2[2];
-    //进程ID
+    // 进程ID
     pid_t pid;
-    //内容存储
+    // 行缓冲区
     char line[MAXLINE];
-    //捕捉信号
+    // 捕捉信号
     if(signal(SIGPIPE,sig_pipe)==SIG_ERR){
         err_sys("signal error");
     }
-    //创建管道
-    if(pipe(fd1)<0||pipe(fd2)<0){
+    // 创建管道
+    if(pipe(fd1)<0||pipe(fd2)<0){ // 失败
         err_sys("pipe error");
     }
-    //创建子进程
-    if((pid=fork())<0){
+    // 创建子进程
+    if((pid=fork())<0){ // 失败
         err_sys("fork error");
     }
-    //父进程
-    else if(pid>0){
-        //关闭读
+    // 父进程
+    else if(pid>0){ /* parent */
+        // 关闭读
         close(fd1[0]);
-        //关闭写
+        // 关闭写
         close(fd2[1]);
-        //读标准输入
+        // 读标准输入
         while(fgets(line,MAXLINE,stdin)!=NULL){
-            //读取长度
+            // 读取长度
             n=strlen(line);
-            //写管道
+            // 写管道
             if(write(fd1[1],line,n)!=n){
                 err_sys("write error to pipe");
             }
-            //读管道
+            // 读管道
             if((n=read(fd2[0],line,MAXLINE))<0){
                 err_sys("read error from pipe");
             }
-            //管道关闭
+            // 管道关闭
             if(0==n){
                 err_msg("child closed pipe");
                 break;
             }
-            //添加结束字符
+            // 添加结束字符
             line[n]=0;
-            //输出
+            // 输出
             if(fputs(line,stdout)==EOF){
                 err_sys("fputs error");
             }
         }
-        //异常检测
+        // 异常检测
         if(ferror(stdin)){
             err_sys("fgets error on stdin");
         }
-        //退出
+        // 退出
         exit(0);
     }
-    //子进程
+    // 子进程
     else{
-        //关闭写
+        // 关闭写
         close(fd1[1]);
-        //关闭读
+        // 关闭读
         close(fd2[0]);
         //绑定标准输入
         if(fd1[0]!=STDIN_FILENO){
