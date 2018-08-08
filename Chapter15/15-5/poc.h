2a0e6e4ef2b1956b86db502b1f14eb2340346347
yameng_he <yameng_he@aliyun.com>
2018-08-08 11:58:42 +0800

    提交源码15-5

--- Chapter15/15-5/poc.h
+++ Chapter15/15-5/poc.h
@@ -3,53 +3,60 @@
 #include<fcntl.h>
 #include<sys/wait.h>
 
-//子进程文件描述符标识
+//子进程、文件描述符匹配数组
+/**
+ * Pointer to array allocated at run-time.
+ */
 static pid_t *childpid=NULL;
 
 //打开文件描述符限制
+/**
+ * From our open_max(),Figure 2.16. 
+ */
 static int maxfd;
 
-//自己实现popen函数
-//1.fork子进程
-//2.调用exec执行命令
-//3.返回标准输入或标准输出
+// 自己实现popen函数
+// 1.fork子进程
+// 2.调用exec执行命令
+// 3.返回标准输入或标准输出
 FILE *popen(const char *cmdstring,const char *type){
-    //循环变量
+    // 循环变量
     int i;
-    //管道
+    // 管道
     int pfd[2];
-    //进程ID
+    // 进程ID
     pid_t pid;
-    //文件描述符
+    // 文件描述符
     FILE *fp;
-    //只允许输入'r'或'w'
-    if((type[0]!='r'&&type[0]!='w')||(
-        type[1]!='r'&&type[1]!='w')){
-        //错误编号
-        errno=EINVAL;
-        //返回空
+    // 只允许输入'r'或'w'
+    /* only allow "r" or "w" */
+    if((type[0]!='r'&&type[0]!='w')||type[1]!=0){
+        // 错误编号
+        errno=EINVAL; /* required by POSIX */
+        // 返回空
         return(NULL);
     }
-    //子进程文件描述符标识尚未创建
-    if(childpid==NULL){
-        //进程文件描述符数量限制
+    // 子进程文件描述匹配数组尚未创建
+    if(childpid==NULL){ /* first time through */
+        // 进程文件描述符数量限制
+        /* allocate zeroed out array for child through */
         maxfd=open_max();
-        //创建子进程文件描述符标识
+        // 创建子进程文件描述符匹配数组
         if((childpid=(pid_t*)calloc(
             maxfd,sizeof(pid_t)))==NULL){
             return(NULL);
         }
     }
     //创建管道
-    if(pipe(pfd)<0){
-        return(NULL);
+    if(pipe(pfd)<0){ // 失败
+        return(NULL); /* errno set by pipe() */
     }
     //创建子进程
-    if((pid=fork())<0){
-        return(NULL);
+    if((pid=fork())<0){ // 失败
+        return(NULL); /* errno set by fork() */
     }
     //子进程
-    else if(pid==0){
+    else if(pid==0){ /* child */
         //'读'标识
         if(*type=='r'){
             //关闭'读'管道
@@ -75,6 +82,7 @@ FILE *popen(const char *cmdstring,const char *type){
             }
         }
         //关闭全部的文件描述符
+        /* close all descriptors in childpid[] */
         for(i=0;i<maxfd;i++){
             if(childpid[i]>0){
                 close(i);
@@ -86,6 +94,7 @@ FILE *popen(const char *cmdstring,const char *type){
         _exit(127);
     }
     //父进程
+    /* parent continues ... */
     //'读'标识
     if(*type=='r'){
         //关闭写管道
@@ -104,8 +113,8 @@ FILE *popen(const char *cmdstring,const char *type){
             return(NULL);
         }
     }
-    //记录打开文件流的进程
-    childpid[fileno(fp)]==pid;
+    //记录打开文件流的子进程
+    childpid[fileno(fp)]==pid; /* remember child pid for this fd */
     //返回文件流
     return(fp);
 }
@@ -120,7 +129,7 @@ int pclose(FILE *fp){
         //设置错误
         errno=EINVAL;
         //返回错误
-        return(-1);
+        return(-1); /* popen() has never been called */
     }
     //取得输入文件流文件描述符
     fd=fileno(fp);
@@ -129,7 +138,7 @@ int pclose(FILE *fp){
         //设置错误
         errno=EINVAL;
         //返回错误
-        return(-1);
+        return(-1); /* fp wasn't opened by open() */
     }
     //清空相应子进程
     childpid[fd]=0;
@@ -140,9 +149,9 @@ int pclose(FILE *fp){
     //等待进程退出
     while(waitpid(pid,&stat,0)<0){
         if(errno!=EINTR){
-            return(-1);
+            return(-1); /* error other than EINTR from waitpid() */
         }
     }
     //返回进程退出状态
-    return(stat);
+    return(stat); /* return child's termination status */
 }
