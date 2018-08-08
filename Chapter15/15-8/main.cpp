a910e67a124562dac66fe3c49a227b5d219ac81a
yameng_he <yameng_he@aliyun.com>
2018-08-08 17:18:26 +0800

    提交源码15-8

--- Chapter15/15-8/main.cpp
+++ Chapter15/15-8/main.cpp
@@ -2,26 +2,26 @@
 
 int main(){
     int n,int1,int2;
-    //输入内容
+    // 行缓冲区
     char line[MAXLINE];
-    //标准输入读
-    while((n==read(STDIN_FILENO,line,MAXLINE))>0){
-        //追加字符串结束标识
+    // 标准输入读取一行
+    while((n=read(STDIN_FILENO,line,MAXLINE))>0){ // 读取成功
+        // 追加字符串结束标识
         line[n]=0;
-        //读取第一个、第二个输入
+        // 格式化读取字符串中的数据
         if(sscanf(line,"%d%d",&int1,&int2)==2){
-            //将和写回line中
+            // 回写行缓冲区中
             sprintf(line,"%d\n",int1+int2);
-            //计算字符串长度
+            // 计算长度
             n=strlen(line);
-            //向标准输出输出字符串
+            // 写标准输出
             if(write(STDOUT_FILENO,line,n)!=n){
                 err_sys("write error");
             }
         }
-        //读取失败
+        // 提取数据失败
         else{
-            //输出错误
+            // 输出错误
             if(write(STDOUT_FILENO,"invalid args\n",13)!=13){
                 err_sys("write error");
             }
