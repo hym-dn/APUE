fbf790638595eaa775af340c0f93f4a3baf5c769
yameng_he <yameng_he@aliyun.com>
2018-08-09 09:38:30 +0800

    提交源码15-10

--- Chapter15/15-10/main.cpp
+++ Chapter15/15-10/main.cpp
@@ -3,16 +3,22 @@
 int main(void){
     int int1,int2;
     char line[MAXLINE];
-    while(fgets(line,MAXLINE,stdin)!=NULL){
+    // 循环从标准输入读
+    while(fgets(line,MAXLINE,stdin)!=0){ // 读取成功
+        // 读取数字成功
         if(sscanf(line,"%d%d",&int1,&int2)==2){
+            // 标准输出
             if(printf("%d\n",int1+int2)==EOF){
                 err_sys("printf error");
             }
-        }else{
+        }
+        // 读取数字失败
+        else{
             if(printf("invalid args\n")==EOF){
                 err_sys("printf error");
             }
         }
     }
+    // 退出
     exit(0);
 }
