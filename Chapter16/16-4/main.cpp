6241131a3e9dbb01367bfc59657e292fc72b3ad4
yameng_he <yameng_he@aliyun.com>
2018-08-21 10:05:31 +0800

    完善代码16-4、16-5

--- Chapter16/16-4/main.cpp
+++ Chapter16/16-4/main.cpp
@@ -1,4 +1,4 @@
-#include"../../apue.h"
+#include"../16-2/connect_retry.h"
 #include<netdb.h>
 #include<errno.h>
 #include<sys/socket.h>
@@ -7,35 +7,33 @@
 #define BUFLEN 128
 #define MAXSLEEP 128
 
-extern int connect_retry(int,const struct sockaddr *,socklen_t);
-
-//打印读取的时间
+// 打印读取的时间
 void print_uptime(int sockfd){
     int n;
     char buf[BUFLEN];
-    //读指定套接字，直到读结束
+    // 读指定套接字，直到读完或出错
     while((n=recv(sockfd,buf,BUFLEN,0))>0){
-        //输出读取内容
+        // 输出读取内容
         write(STDOUT_FILENO,buf,n);
     }
-    //如果读失败
+    // 如果读失败
     if(n<0){
         err_sys("recv error");
     }
 }
 
 int main(int argc,char *argv[]){
-    //映射地址
+    // 映射地址
     struct addrinfo *ailist,*aip;
-    //过滤模板
+    // 过滤模板
     struct addrinfo hint;
-    //套接字、错误
+    // 套接字、错误
     int sockfd,err;
-    //如果参数非法
+    // 如果参数非法
     if(argc!=2){
         err_quit("usage: ruptime hostname");
     }
-    //设置过滤模板
+    // 设置过滤模板
     hint.ai_flags=0;
     hint.ai_family=0;
     hint.ai_socktype=SOCK_STREAM;
@@ -44,53 +42,30 @@ int main(int argc,char *argv[]){
     hint.ai_canonname=NULL;
     hint.ai_addr=NULL;
     hint.ai_next=NULL;
-    //将主机名、服务映射到一个地址
+    // 将主机名、服务映射到一个地址
     if((err=getaddrinfo(argv[1],"ruptime",&hint,&ailist))!=0){
         err_quit("getaddrinfo error: %s",gai_strerror(err));
     }
-    //遍历地址
+    // 遍历地址
     for(aip=ailist;aip!=NULL;aip=aip->ai_next){
-        //创建套接字
+        // 创建套接字
         if((sockfd=socket(aip->ai_family,SOCK_STREAM,0))<0){
             err=errno;
         }
-        //连接失败
+        // 连接失败
         if(connect_retry(sockfd,aip->ai_addr,aip->ai_addrlen)<0){
             err=errno;
         }
-        //连接成功
+        // 连接成功
         else{
-            //打印时间
+            // 打印时间
             print_uptime(sockfd);
-            //退出
+            // 退出
             exit(0);
         }
     }
-    //打印错误
+    // 打印错误
     fprintf(stderr,"can't connect to %s: %s\n",argv[1],strerror(err));
-    //退出
+    // 退出
     exit(1);
 }
-
-int connect_retry(int sockfd,const struct sockaddr *addr,socklen_t alen){
-    //定义秒
-    int nsec;
-    //指数补偿(每循环一次增加每次尝试的延迟，
-    //直到最大延迟为2分钟)
-    for(nsec=1;nsec<=MAXSLEEP;nsec<<=1){
-        //连接指定服务器
-        if(connect(sockfd,addr,alen)==0){
-            return(0);
-        }
-        //延时重试
-        if(nsec<=MAXSLEEP/2){
-            sleep(nsec);
-        }
-    }
-    //返回
-    return(-1);
-}
-
-
-
-
--- Chapter16/16-5/main.cpp
+++ Chapter16/16-5/main.cpp
@@ -1,4 +1,4 @@
-#include"../../apue.h"
+#include"../16-3/initserver.h"
 #include<netdb.h>
 #include<errno.h>
 #include<syslog.h>
@@ -11,46 +11,44 @@
 #define HOST_NAME_MAX 256
 #endif
 
-extern int initserver(int,const struct sockaddr *,socklen_t,int);
-
-//实现服务器套接字处理
+// 实现服务器套接字处理
 void serve(int sockfd){
-    //套接字
+    // 客户端套接字
     int clfd;
-    //文件描述符
+    // 管道文件描述符
     FILE *fp;
-    //缓冲区
+    // 缓冲区
     char buf[BUFLEN];
-    //循环处理套接字
+    // 循环处理套接字
     for(;;){
-        //等待套接字连接
+        // 等待套接字连接
         clfd=accept(sockfd,NULL,NULL);
-        //如果等待失败
+        // 如果等待失败
         if(clfd<0){
-            //书写日志
+            // 书写日志
             syslog(LOG_ERR,"ruptimed: accept error: %s",
                 strerror(errno));
-            //退出
+            // 退出
             exit(1);
         }
-        //创建管道,子进程、执行命令，并将执行结果返回到fp中，失败
+        // 创建管道,子进程、执行命令，并将执行结果返回到fp中，失败
         if((fp=popen("/usr/bin/uptime","r"))==NULL){
-            //形成错误描述
+            // 形成错误描述
             sprintf(buf,"error: %s\n",strerror(errno));
-            //发送错误描述
+            // 发送错误描述
             send(clfd,buf,strlen(buf),0);
         }
-        //创建管道、子进程执行命令，并将执行结果返回到fp中，成功
+        // 创建管道、子进程执行命令，并将执行结果返回到fp中，成功
         else{
-            //循环读取执行结果
+            // 从管道中循环读取执行结果
             while(fgets(buf,BUFLEN,fp)!=NULL){
-                //发送执行结果
+                // 发送执行结果
                 send(clfd,buf,strlen(buf),0);
             }
-            //关闭文件描述符
+            // 关闭文件描述符
             pclose(fp);
         }
-        //关闭套接字
+        // 关闭套接字
         close(clfd);
     }
 }
@@ -60,29 +58,29 @@ int main(int argc,char * /*argv*/ []){
     struct addrinfo hint;
     int sockfd,err,n;
     char *host;
-    //参数非法
+    // 参数非法
     if(argc!=1){
         err_quit("usage: ruptimed");
     }
-    //换算主机名称最大长度
+    // 换算主机名称最大长度
 #ifdef _SC_HOST_NAME_MAX
     n=sysconf(_SC_HOST_NAME_MAX);
     if(n<0)
 #endif
       n=HOST_NAME_MAX;
-    //主机缓冲区
+    // 主机缓冲区
     host=(char*)malloc(n);
-    //分配失败
+    // 分配失败
     if(host==NULL){
         err_sys("malloc error");
     }
-    //获取主机名称
+    // 获取主机名称
     if(gethostname(host,n)<0){
         err_sys("gethostname error");
     }
     //
     //daemonize("ruptimed");
-    //过滤模板
+    // 过滤模板
     hint.ai_flags=AI_CANONNAME;
     hint.ai_family=0;
     hint.ai_socktype=0;
@@ -91,62 +89,24 @@ int main(int argc,char * /*argv*/ []){
     hint.ai_canonname=NULL;
     hint.ai_addr=NULL;
     hint.ai_next=NULL;
-    //利用主机名称、服务名称获取相关地址列表
+    // 利用主机名称、服务名称获取相关地址列表
     if((err=getaddrinfo(host,"ruptime",&hint,&ailist))!=0){
-        //错误日志
+        // 错误日志
         syslog(LOG_ERR,"ruptimed: getaddrinfo error: %s",gai_strerror(err));
-        //退出
+        // 退出
         exit(1);
     }
-    //遍历地址列表创建服务
+    // 遍历地址列表创建服务
     for(aip=ailist;aip!=NULL;aip=aip->ai_next){
-        //初始化套接字
+        // 初始化套接字
         if((sockfd=initserver(SOCK_STREAM,aip->ai_addr,
             aip->ai_addrlen,QLEN))>=0){
-            //等待接受套接字
+            // 等待接受套接字
             serve(sockfd);
-            //退出程序
+            // 退出程序
             exit(0);
         }
     }
-    //退出
+    // 退出
     exit(1);
-}
-
-
-/**
- * type - 套接子类型。
- * addr - 地址信息。
- * alen - 地址信息尺寸。
- * qlen - 允许的连接数。
- */ 
-int initserver(int type,const struct sockaddr *addr,socklen_t alen,int qlen){
-    //记录套接字
-    int fd;
-    //记录错误
-    int err=0;
-    //创建一个套接子
-    if((fd=socket(addr->sa_family,type,0))<0){
-        return(-1);
-    }
-    //绑定地址
-    if(bind(fd,addr,alen)<0){
-        err=errno;
-        goto errout;
-    }
-    //如果套接字面向连接
-    if(SOCK_STREAM==type||SOCK_SEQPACKET==type){
-        //监听套接字
-        if(listen(fd,qlen)<0){
-            err=errno;
-            goto errout;
-        }
-    }
-    //返回套接字
-    return(fd);
-//错误处理
-errout:
-    close(fd);
-    errno=err;
-    return(-1);
 }
