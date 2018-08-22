032eb8c77313edbe409addb2a81127b5f4a312c0
yameng_he <yameng_he@aliyun.com>
2018-08-22 14:12:06 +0800

    完善源码17-6-1、17-6-2

--- Chapter17/17-6-1/main.cpp
+++ Chapter17/17-6-1/main.cpp
@@ -1,46 +1,50 @@
+/**
+ * 利用UNIX套接子域协助消息队列使用select、poll。
+ */
+
 #include"../../apue.h"
 #include<poll.h>
 #include<pthread.h>
 #include<sys/msg.h>
 #include<sys/socket.h>
 
-//消息队列个数
+// 消息队列个数
 #define NQ 3
-//最大消息尺寸
+// 最大消息尺寸
 #define MAXMSZ 512
-//消息队列基础主键
+// 消息队列基础主键
 #define KEY 0x123
 
-//线程信息
+// 线程参数
 struct threadinfo{
-    //消息队列ID
-    int qid;
-    int fd;
+    int qid; // 消息队列ID
+    int fd; // UNIX域套接子
 };
 
-//自定义消息
+// 自定义消息
 struct mymesg{
-    //消息类型
+    // 消息类型
     long mtype;
-    //消息数据
+    // 消息数据
     char mtext[MAXMSZ];
 };
 
+// 线程函数
 void *helper(void *arg){
     int n;
-    //消息
+    // 消息
     struct mymesg m;
-    //线程信息
+    // 线程信息
     struct threadinfo *tip=(struct threadinfo*)arg;
-    //循环
+    // 循环
     for(;;){
-        //清空消息
+        // 清空消息
         memset(&m,0,sizeof(m));
-        //接收消息
+        // 接收消息
         if((n=msgrcv(tip->qid,&m,MAXMSZ,0,MSG_NOERROR))<0){
             err_sys("msgrcv error");
         }
-        //写套接字
+        // 写套接字
         if(write(tip->fd,m.mtext,n)<0){
             err_sys("write error");
         }
@@ -49,70 +53,70 @@ void *helper(void *arg){
 
 int main(){
     int i,n,err;
-    //UNIX阈报文套接字
+    // UNIX域套接字
     int fd[2];
-    //队列ID
+    // 队列ID
     int qid[NQ];
-    //poll监听结构体
+    // poll监听结构体
     struct pollfd pfd[NQ];
-    //线程信息
+    // 线程信息
     struct threadinfo ti[NQ];
-    //线程ID
+    // 线程ID
     pthread_t tid[NQ];
-    //读取缓冲区
+    // 读取缓冲区
     char buf[MAXMSZ];
-    //循环建立消息队列
+    // 循环建立消息队列
     for(i=0;i<NQ;i++){
-        //创建或打开消息队列
-        //KEY+i 消息队列主键
-        //IPC_CREAT创建
-        //0666 用户读写、组读写、其他读写
-        //返回消息ID
+        // 创建或打开消息队列
+        // KEY+i 消息队列主键
+        // IPC_CREAT创建
+        // 0666 用户读写、组读写、其他读写
+        // 返回消息ID
         if((qid[i]=msgget((KEY+i),IPC_CREAT|0666))<0){
             err_sys("mesgget error");
         }
-        //打印队列ID
+        // 打印队列ID
         printf("queue ID %d is %d\n",i,qid[i]);
-        //创建UNIX阈报文套接字
-        if(socketpair(AF_UNIX,SOCK_DGRAM,0,fd)<0){
+        // 创建UNIX阈报文套接字
+        if(socketpair(AF_UNIX,SOCK_DGRAM,0,fd)<0){ // 报文方式创建
             err_sys("socketpair error");
         }
-        //设置poll结构体
-        //套接字
-        pfd[i].fd=fd[0];
-        //事件
+        // 设置poll结构体
+        // 套接字
+        pfd[i].fd=fd[0]; // 读
+        // 监听读取
         pfd[i].events=POLLIN;
-        //设置线程信息
-        //队列ID
+        // 设置线程信息
+        // 队列ID
         ti[i].qid=qid[i];
-        //套接字
-        ti[i].fd=fd[1];
-        //启动线程
+        // 套接字
+        ti[i].fd=fd[1]; // 写
+        // 启动线程
         if((err=pthread_create(&tid[i],NULL,helper,&ti[i]))!=0){
             err_exit(err,"pthread_create error");
         }
     }
-    //循环接收消息
+    // 循环接收消息
     for(;;){
-        //监听套接字
+        // 监听套接字
         if(poll(pfd,NQ,-1)<0){
             err_sys("poll error");
         }
-        //循环读取消息队列
+        // 循环读取消息队列
         for(i=0;i<NQ;i++){
-            //如果读事件触发
+            // 如果读事件触发
             if(pfd[i].revents&POLLIN){
-                //读套接字
+                // 读套接字
                 if((n=read(pfd[i].fd,buf,sizeof(buf)))<0){
                     err_sys("read error");
                 }
-                //追加结束符
+                // 追加结束符
                 buf[n]=0;
-                //显示数据
+                // 显示数据
                 printf("queue id %d,message %s\n",qid[i],buf);
             }
         }
     }
-    //退出
+    // 退出
     exit(0);
 }
--- Chapter17/17-6-2/main.cpp
+++ Chapter17/17-6-2/main.cpp
@@ -1,49 +1,48 @@
 #include"../../apue.h"
 #include<sys/msg.h>
 
-//最大消息尺寸
+// 最大消息尺寸
 #define MAXMSZ 512
 
-//消息结构体
+// 消息结构体
 struct mymesg{
-    //消息类型
+    // 消息类型
     long mtype;
-    //消息数据
+    // 消息数据
     char mtext[MAXMSZ];
 };
 
 int main(int argc,char *argv[]){
-    //消息队列主键
+    // 消息队列主键
     key_t key;
-    //消息队列ID
+    // 消息队列ID
     long qid;
-    //字节数
+    // 字节数
     size_t nbytes;
-    //消息
+    // 消息
     struct mymesg m;
-    //输入参数非法
+    // 输入参数非法
     if(argc!=3){
         fprintf(stderr,"usage: sendmsg KEY message\n");
     }
-    //获取消息队列主键
+    // 获取消息队列主键
     key=strtol(argv[1],NULL,0);
-    //打开消息队列
+    // 打开消息队列
     if((qid=msgget(key,0))<0){
         err_sys("can't open queue key %s",argv[1]);
     }
-    //初始化消息
+    // 初始化消息
     memset(&m,0,sizeof(m));
-    //填充消息
+    // 填充消息
     strncpy(m.mtext,argv[2],MAXMSZ-1);
-    //计算字节数
+    // 计算字节数
     nbytes=strlen(m.mtext);
-    //消息类型
+    // 消息类型
     m.mtype=1;
-    //发送消息
+    // 发送消息
     if(msgsnd(qid,&m,nbytes,0)<0){
         err_sys("can't send message");
     }
-    //退出
+    // 退出
     exit(0);
 }
-
