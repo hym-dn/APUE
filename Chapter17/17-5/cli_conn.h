677b657355da95c20bc38534ae140b613203e7bf
yameng_he <yameng_he@aliyun.com>
2018-08-22 13:15:33 +0800

    完善源码17-5

--- Chapter17/17-5/cli_conn.h
+++ Chapter17/17-5/cli_conn.h
@@ -8,20 +8,24 @@
  * 如果函数执行成功返回管道文件描述符，
  * 否则返回值小于0。
  */
+/**
+ * Create a client endpoint and connect to a server.
+ * Returns fd if all OK,<0 on error.
+ */
 int cli_conn(const char *name){
-    //文件描述符
+    // 文件描述符
     int fd;
-    //打开指定的管道
+    // 打开指定的管道
     if((fd=open(name,O_RDWR))<0){
         return(-1);
     }
-    //检测是否为流设备
+    // 检测是否为流设备
     if(isastream(fd)==0){
-        //关闭文件描述符
+        // 关闭文件描述符
         close(fd);
-        //返回错误
+        // 返回错误
         return(-2);
     }
-    //返回文件描述符
+    // 返回文件描述符
     return(fd);
 }
