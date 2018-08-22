a921c3e21e22394dce64dea22782c3893f03e0bb
yameng_he <yameng_he@aliyun.com>
2018-08-22 13:39:12 +0800

    完善源码17-6

--- Chapter17/17-6/s_pipe.h
+++ Chapter17/17-6/s_pipe.h
@@ -4,7 +4,10 @@
 /**
  * 利用UNIX域套接字创建管道。
  */
+/**
+ * Returns a full-duplex "stream" pipe (a UNIX domain socket)
+ * with the two file descriptors returned in fd[0] and fd[1]. 
+ */
 int s_pipe(int fd[2]){
     return(socketpair(AF_UNIX,SOCK_STREAM,0,fd));
 }
-
