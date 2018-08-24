54b0f4eeb10df0325c3d051e1cffac7f90d77699
yameng_he <yameng_he@aliyun.com>
2018-08-24 15:30:52 +0800

    完善源码17-21

--- Chapter17/17-21/opend.h
+++ Chapter17/17-21/opend.h
@@ -1,13 +1,14 @@
 #include"../../apue.h"
 #include<errno.h>
 
-#define<errno.h>
+#define CL_OPEN "open" /* client's request for server */
 
-#define CL_OPEN "open"
-
-extern char errmsg[];
-extern int oflag;
-extern char *pathname;
+// 错误消息
+extern char errmsg[]; /* error message string to return to client */
+// 打开标志
+extern int oflag; /* open() flag: O_XXX ... */
+// 打开文件路径
+extern char *pathname; /* of file t open() for client */
 
 int cli_args(int,char**);
 void handle_request(char*,int,int);
