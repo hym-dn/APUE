681a74de44c6da355d51a0201c849cb4f59c9a83
yameng_he <yameng_he@aliyun.com>
2018-08-24 17:21:02 +0800

    提交源码17-25

---
+++ Chapter17/17-25/cli_args.h
@@ -0,0 +1,23 @@
+#include"opend.h"
+
+/**
+ * This function is called by buf_args(),which is called by 
+ * request().buf_args() has broken up the client's buffer 
+ * into an argv[]-style array, which we now process.
+ */
+int cli_args(int argc,char **argv){
+    // 参数不合法
+    if(argc!=3||strcmp(argv[0],CL_OPEN)!=0){
+        // 错误消息
+        strcpy(errmsg,"usage: <pathname><oflag>\n");
+        // 返回
+        return(-1);
+    }
+    // 路径
+    pathname=argv[1]; /* save ptr to pathname to open */
+    // 标志
+    oflag=atoi(argv[2]);
+    // 返回
+    return(0);
+}
+
