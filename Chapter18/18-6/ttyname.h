7f58af7b36c58b904a04cecbc01b307f154b8c4e
yameng_he <yameng_he@aliyun.com>
2018-08-28 16:51:56 +0800

    完善源码18-6

--- Chapter18/18-6/ttyname.h
+++ Chapter18/18-6/ttyname.h
@@ -6,159 +6,169 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-//设备驱动信息
+// 目录信息结构体（链表）
 struct devdir{
     struct devdir *d_next;
     char *d_name;
 };
 
-//设备驱动链表头
+// 目录链表头
 static struct devdir *head;
-//设备驱动链表尾
+// 目录链表尾
 static struct devdir *tail;
-//路径名称
+// 目录缓冲区
 static char pathname[_POSIX_PATH_MAX+1];
 
-//向链表中添加驱动信息
+// 向链表中添加目录信息
 static void add(char *dirname){
+    // 目录信息结构体
     struct devdir *ddp;
     int len;
-    //路径长度
+    // 路径长度
     len=strlen(dirname);
-    //过滤.路径以及..路径
+    // 过滤.路径以及..路径
+    /**
+     * Skip.,..,and /dev/fd.
+     */
     if((dirname[len-1]=='.')&&(dirname[len-2]=='/'||
        (dirname[len-2]=='.'&&dirname[len-3]=='/'))){
         return;
     }
-    //过滤"/dev/fd"路径
+    // 过滤"/dev/fd"路径
     if(strcmp(dirname,"dev/fd")==0){
         return;
     }
-    //创建驱动信息结构体
+    // 创建目录信息结构体
     ddp=(struct devdir *)malloc(sizeof(struct devdir));
-    if(ddp==NULL){
+    if(ddp==NULL){ // 失败
         return;
     }
-    //拷贝路径名称
+    // 拷贝路径名称
     ddp->d_name=strdup(dirname);
     if(ddp->d_name==NULL){
         free(ddp);
         return;
     }
-    //设置next指针
+    // 设置next指针
     ddp->d_next=NULL;
-    //如果尾指针为空
+    // 如果尾指针为空
     if(tail==NULL){
         head=ddp;
         tail=ddp;
     }
-    //如果尾指针非空
+    // 如果尾指针非空
     else{
         tail->d_next=ddp;
         tail=ddp;
     }
 }
 
-//清除链表
+// 清除链表
 static void cleanup(){
     struct devdir *ddp,*nddp;
-    //链表头
+    // 链表头
     ddp=head;
-    //表头存在
+    // 表头存在
     while(ddp!=NULL){
-        //一下个
+        // 一下个
         nddp=ddp->d_next;
-        //释放当前名称
+        // 释放当前名称
         free(ddp->d_name);
-        //释放当前结构
+        // 释放当前结构
         free(ddp);
-        //变为下一个
+        // 变为下一个
         ddp=nddp;
     }
-    //重置链表头、尾
+    // 重置链表头、尾
     head=NULL;
     tail=NULL;
 }
 
-//搜索路径
+// 搜索指定目录
 static char *searchdir(const char *dirname,struct stat *fdstatp){
-    //
+    // 文件、目录属性
     struct stat devstat;
     DIR *dp;
     int devlen;
     struct dirent *dirp;
-    //拷贝路径名称
+    // 记录搜索目录名称
     strcpy(pathname,dirname);
-    //打开路径
-    if((dp=opendir(dirname))==NULL){
+    // 打开目录
+    if((dp=opendir(dirname))==NULL){ // 失败
         return(NULL);
     }
-    //拼接路径
+    // 拼接目录名称
     strcat(pathname,"/");
-    //路径长度
+    // 目录长度
     devlen=strlen(pathname);
-    //读取路径
+    // 遍历当前目录
     while((dirp=readdir(dp))!=NULL){
-        //形成完整的文件名称
+        // 拼接完整路径名称
         strncpy(pathname+devlen,dirp->d_name,
             _POSIX_PATH_MAX-devlen);
-        //跳过标准输入、标准输出
+        // 跳过标准输入、标准输出
+        /**
+         * Skip aliases.
+         */
         if(strcmp(pathname,"/dev/stdin")==0||
            strcmp(pathname,"/dev/stdout")==0||
            strcmp(pathname,"/dev/stderr")==0){
             continue;
         }
-        //获取文件属性
+        // 获取目录/文件属性
         if(stat(pathname,&devstat)<0){
             continue;
         }
-        //如果当前是路径
+        // 如果为目录
         if(S_ISDIR(devstat.st_mode)){
-            add(pathname);
+            add(pathname); // 将目录信息添加到链表中
             continue;
         }
-        //发现匹配
+        // 发现指定文件
         if(devstat.st_ino==fdstatp->st_ino&&
            devstat.st_dev==fdstatp->st_dev){
-            closedir(dp);
-            return(pathname);
+            closedir(dp); // 关闭目录
+            return(pathname); // 返回名称
         }
     }
-    //关闭路径
+    // 关闭路径
     closedir(dp);
-    //返回空
+    // 返回空
     return(NULL);
 }
 
-//获取指定文件描述符名称
+// 获取指定文件描述符的终端设备名称
 char *ttyname(int fd){
+    // 文件属性
     struct stat fdstat;
     struct devdir *ddp;
     char *rval;
-    //不是终端设备
+    // 不是终端设备
     if(isatty(fd)==0){
         return(NULL);
     }
-    //获取文件属性
+    // 获取文件属性
     if(fstat(fd,&fdstat)<0){
         return(NULL);
     }
-    //非字符设备
+    // 非字符设备
     if(S_ISCHR(fdstat.st_mode)==0){
         return(NULL);
     }
-    //查找指定目录
+    // 查找指定目录
     rval=searchdir("/dev",&fdstat);
-    //如果没有找到
+    // 尚未找到指定文件
     if(rval==NULL){
+        // 遍历目录链表
         for(ddp=head;ddp!=NULL;ddp=ddp->d_next){
+            // 搜索子目录，直到搜到为止
             if((rval=searchdir(ddp->d_name,&fdstat))!=NULL){
                 break;
             }
         }
     }
-    //清除链表
+    // 清除链表
     cleanup();
-    //返回名称
+    // 返回名称
     return(rval);
 }
