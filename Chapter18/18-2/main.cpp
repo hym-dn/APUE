ab4a71ef62302d27d33cc733d23cc1191445283b
yameng_he <yameng_he@aliyun.com>
2018-08-28 13:40:44 +0800

    提交源码18-2

--- Chapter18/18-2/main.cpp
+++ Chapter18/18-2/main.cpp
@@ -1,14 +1,15 @@
 #include"../../apue.h"
 #include<termios.h>
 
+// 使用屏蔽标志获取或设置一个值
 int main(void){
-    //定义终端信息结构体
+    // 终端属性结构体
     struct termios term;
-    //获取标准输入的终端信息
-    if(tcgetattr(STDIN_FILENO,&term)<0){
+    // 获取标准输入的终端属性
+    if(tcgetattr(STDIN_FILENO,&term)<0){ // 失败
         err_sys("tcgetattr error");
     }
-    //打印当前终端数据位数
+    // 打印终端字符
     switch(term.c_cflag&CSIZE){
     case CS5:
         printf("5 bits/byte\n");
@@ -25,15 +26,14 @@ int main(void){
     default:
         printf("unknown bits/byte\n");
     }
-    //清除数据位长度
-    term.c_cflag&=~CSIZE;
-    //设置8位数据位
-    term.c_cflag|=CS8;
-    //设置当前标准输入终端信息
+    // 清除全部字符长度标志
+    term.c_cflag&=~CSIZE; /* zero out the bits */
+    // 设置8位字符长度
+    term.c_cflag|=CS8; /* set 8 bits/byte */
+    // 设置当前标准输入终端属性
     if(tcsetattr(STDIN_FILENO,TCSANOW,&term)<0){
         err_sys("tcsetattr error");
     }
-    //退出
+    // 退出
     exit(0);
 }
-
