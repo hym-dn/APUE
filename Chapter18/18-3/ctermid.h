ed8649ed2b10b8a0d5d02391f1e81b351ff691d1
yameng_he <yameng_he@aliyun.com>
2018-08-28 15:53:18 +0800

    提交源码18-3

--- Chapter18/18-3/ctermid.h
+++ Chapter18/18-3/ctermid.h
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<string.h>
 
+// 终端名称缓冲区
 static char ctermid_name[L_ctermid];
 
+// 获取进程终端名称
 char *ctermid(char *str){
     if(str==NULL){
         str=ctermid_name;
     }
-    return(strcpy(str,"/dev/tty"));
+    return(strcpy(str,"/dev/tty")); /* strcpy() return str */
 }
