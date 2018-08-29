02d3e2c415cffc675c0d7ba08281e65df6008367
yameng_he <yameng_he@aliyun.com>
2018-08-29 10:58:44 +0800

    提交源码18-9

--- Chapter18/18-9/main.cpp
+++ Chapter18/18-9/main.cpp
@@ -1,60 +1,5 @@
 #include"../../apue.h"
-#include<signal.h>
-#include<stdio.h>
-#include<termios.h>
-
-//最大密码长度
-#define MAX_PASS_LEN 8
-
-char *getpass(const char *prompt){
-    static char buf[MAX_PASS_LEN+1];
-    char *ptr;
-    sigset_t sig,osig;
-    struct termios ts,ots;
-    FILE *fp;
-    int c;
-    //打开当前进程使用的终端
-    if((fp=fopen(ctermid(NULL),"r+"))==NULL){
-        return(NULL);
-    }
-    //清空终端缓冲区
-    setbuf(fp,NULL);
-    //清空信号
-    sigemptyset(&sig);
-    //添加信号
-    sigaddset(&sig,SIGINT);
-    sigaddset(&sig,SIGTSTP);
-    //阻塞信号
-    sigprocmask(SIG_BLOCK,&sig,&osig);
-    //获取终端信息
-    tcgetattr(fileno(fp),&ts);
-    //保存终端信息
-    ots=ts;
-    //不进行回显
-    ts.c_lflag&=~(ECHO|ECHOE|ECHOK|ECHONL);
-    //设置终端信息
-    tcsetattr(fileno(fp),TCSAFLUSH,&ts);
-    //向终端输入提示
-    fputs(prompt,fp);
-    //循环从终端读取输入
-    ptr=buf;
-    while((c=getc(fp))!=EOF&&c!='\n'){
-        if(ptr<&buf[MAX_PASS_LEN]){
-            *ptr++=c;
-        }
-    }
-    *ptr=0;
-    //输出换行
-    putc('\n',fp);
-    //恢复终端属性
-    tcsetattr(fileno(fp),TCSAFLUSH,&ots);
-    //恢复信号屏蔽
-    sigprocmask(SIG_SETMASK,&osig,NULL);
-    //关闭终端流
-    fclose(fp);
-    //返回读取密码
-    return(buf);
-}
+#include"../18-8/getpass.h"
 
 int main(void){
     char *ptr;
@@ -62,8 +7,9 @@ int main(void){
         err_sys("getpass error");
     }
     printf("password: %s\n",ptr);
+    /* now use password (probably encrypt it) ... */
     while(*ptr!=0){
-        *ptr++=0;
+        *ptr++=0; /* zero it out when we're done with it */
     }
     exit(0);
 }
