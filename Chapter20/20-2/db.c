102e4b7c19d584d75cc7483e1d9a1d3e958d3ef8
yameng_he <yameng_he@aliyun.com>
2018-09-05 16:46:49 +0800

    提交源码20-2

--- Chapter20/20-2/db.c
+++ Chapter20/20-2/db.c
@@ -850,3 +850,58 @@ doreturn: /* unlock hash chain locked by _db_find_and_lock */
     }
     return(rc);
 }
+
+/**
+ * Try to find a free index record and accompanying data record
+ * of the correct sizes. We're only called by db_store.
+ */
+static int _db_findfree(DB *db,int keylen,int datlen){
+    int rc;
+    off_t offset,nextoffset,saveoffset;
+    /**
+     * Lock the free list.
+     */
+    if(writew_lock(db->idxfd,FREE_OFF,SEEK_SET,1)<0){
+        err_dump("_db_findfree: writew_lock error");
+    }
+    /**
+     * Read the free list pointer.
+     */
+    saveoffset=FREE_OFF;
+    offset=_db_readptr(db,saveoffset);
+    while(offset!=0){
+        nextoffset=_db_readidx(db,offset);
+        if(strlen(db->idxbuf)==keylen&&db->datlen==datlen){
+            break; /* found a match */
+        }
+        saveoffset=offset;
+        offset=nextoffset;
+    }
+    if(offset==0){
+        rc=-1; /* no match found */
+    }else{
+        /**
+         * Found a free record with mathing sizes.
+         * The index record was read in by _db_readidx above.
+         * which sets db->ptrval. Also, saveoffset points to
+         * the chain ptr that pointed to this empty record on
+         * the free list. We set this chain ptr to db->ptrval,
+         * which removes the empty record from the free list.
+         */
+        _db_writeptr(db,saveoffset,db->ptrval);
+        rc=0;
+        /**
+         * Notice also that _db_readidx set both db->idxoff
+         * and db->datoff. This is used by the caller, db_store,
+         * to write the new index record and data record.
+         */
+    }
+    /**
+     * Unlock the free list.
+     */
+    if(un_lock(db->idxfd,FREE_OFF,SEEK_SET,1)<0){
+        err_dump("_db_findfree: un_lock error");
+    }
+    // 返回
+    return(rc);
+}
